eaa4fec7e2fd4d5643f4400b37d8ac6ccb5850b7
Alex Gajewski <agajews@gmail.com>
2016-05-26 12:37:03 -0500

    Initial commit

---
+++ api.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+from neural_models.music_recommendator.audio_model import AudioModel
+
+app = Flask(__name__)
+api = Api(app)
+
+todos = {}
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
