0936df7c6f128608c88002ffd1665c1314f5379d
Alex Gajewski <agajews@gmail.com>
2016-05-26 20:43:18 -0500

    Added user creation

--- api.py
+++ api.py
@@ -1,15 +1,45 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_restful.reqparse import RequestParser
+
+from neural_models.music_recommendator.test_audio_model import User, Song
 
 app = Flask(__name__)
 api = Api(app)
 
+user_parser = RequestParser()
+user_parser.add_argument('user_id', type=int)
+user_parser.add_argument('song_name', type=str, action='append')
+user_parser.add_argument('song_artist', type=str, action='append')
+user_parser.add_argument('play_count', type=str, action='append')
 
-class HelloWorld(Resource):
+users = {}
+
+
+class AddUser(Resource):
+
     def get(self):
-        return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+        return {'users': str(users)}
+
+    def post(self):
+
+        args = user_parser.parse_args()
+        user_id = args['user_id']
+
+        user_songs = []
+
+        for name, artist, play_count in zip(
+                args['song_name'],
+                args['song_artist'],
+                args['play_count']):
+
+            song = Song(name, artist, play_count=play_count)
+            user_songs.append(song)
+
+        users.append(User(user_id, user_songs))
+
+api.add_resource(AddUser, '/adduser')
 
 if __name__ == '__main__':
     app.run(debug=True)
