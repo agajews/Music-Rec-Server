7a29ef2f279403cef3bae000e44151fd40562019
Alex Gajewski <agajews@gmail.com>
2016-05-26 19:19:41 -0500

    Switched to hello world api

--- api.py
+++ api.py
@@ -1,22 +1,15 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Resource, Api
-
-from neural_models.music_recommendator.audio_model import AudioModel
 
 app = Flask(__name__)
 api = Api(app)
 
-todos = {}
 
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
-
-api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- run.sh
+++ run.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+# Install dependencies
+sudo add-apt-repository -y ppa:fkrull/deadsnakes
+sudo apt-get update
+sudo apt-get install -y git python3.5 python3.5-dev python-pip python-dev build-essential gfortran libopenblas-dev liblapack-dev
+# sudo apt-get install git gcc python34 python34-devel gcc-gfortran lapack lapack-devel blas blas-devel gcc-c++ -y
+
+# Enable swap
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+sudo /sbin/mkswap /var/swap.1
+sudo /sbin/swapon /var/swap.1
+
+# Get server source
 git clone https://github.com/agajews/Music-Rec-Server.git
 cd Music-Rec-Server
-python api.py
+
+# Setup virtualenvwrapper
+sudo pip install virtualenvwrapper
+source /usr/local/bin/virtualenvwrapper.sh
+export WORKON_HOME=~/envs
+
+# Enter virtualenv
+mkvirtualenv -p python3.5 music-rec-server
+workon music-rec-server
+
+# Install python dependencies
+pip install numpy
+pip install -r requirements.txt
+
+# Disable swap
+sudo swapoff /var/swap.1
+sudo rm /var/swap.1
+
+# Run server
+# python3.5 api.py
+~/envs/music-rec-server/bin/gunicorn api:app -p api.pid -b 0.0.0.0:8000 -D
